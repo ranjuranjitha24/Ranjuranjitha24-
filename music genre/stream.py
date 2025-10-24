bc2e1b1e57b93495ea6ea45a21e062a67e701555
ranjuranjitha24 <155605537+ranjuranjitha24@users.noreply.github.com>
2025-10-24 20:36:02 +0530

    Add files via upload

---
+++ music genre/stream.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write("hi streamlit")
+st.subheader("hello world")
+st.selectbox("Which programming language do you like?", ["Python", "Java", "C++", "JavaScript"])
+st.checkbox("python")
